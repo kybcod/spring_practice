ee22a1a66e5f71288ca025a5c05a1c9dd0aaf782
김예빈 <kyb-0215@naver.com>
2025-03-25 09:57:20 +0900

    ObjectMapper

--- servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -12,19 +12,21 @@ import java.io.IOException;
 
 @WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
+
     private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse
-            response)
-            throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Content-Type: application/json
         response.setHeader("content-type", "application/json");
         response.setCharacterEncoding("utf-8");
+
         HelloData data = new HelloData();
         data.setUsername("kim");
         data.setAge(20);
+
         //{"username":"kim","age":20}
-        String result = objectMapper.writeValueAsString(data);
+        String result = objectMapper.writeValueAsString(data); //Java 객체 → JSON 문자열 변환
         response.getWriter().write(result);
     }
 }
