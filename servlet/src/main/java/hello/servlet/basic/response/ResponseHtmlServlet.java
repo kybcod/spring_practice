024754e93ffe691237215bedd87c308ea4d3b669
김예빈 <kyb-0215@naver.com>
2025-03-26 15:48:18 +0900

    HTML 응답

--- servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -11,18 +11,21 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
 public class ResponseHtmlServlet extends HttpServlet {
+
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse
-            response)
-            throws ServletException, IOException {
-        //Content-Type: text/html;charset=utf-8
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // HTML 응답을 반환
         response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
+
+        // HTTP 응답을 작성
         PrintWriter writer = response.getWriter();
         writer.println("<html>");
         writer.println("<body>");
         writer.println(" <div>안녕?</div>");
         writer.println("</body>");
         writer.println("</html>");
+
     }
 }
