691f56a00b9ab7b650896702ff272acb549baa25
kyb <kyb-0215@naver.com>
2025-07-14 20:10:19 +0900

    form-start 파일 삭제 및 추가

---
+++ form-start/src/main/java/com/hello/formstart/FormStartApplication.java
@@ -0,0 +1,13 @@
+package com.hello.formstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormStartApplication.class, args);
+	}
+
+}
---
+++ form-start/src/test/java/com/hello/formstart/FormStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.formstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
