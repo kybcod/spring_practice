b01359c95f7142198abbbea582240dd5710805fe
김예빈 <kyb-0215@naver.com>
2025-07-29 13:27:17 +0900

    MessageCodesResolver, ValidationUtils

--- validation/src/main/java/hello/validation/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/validation/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -167,11 +168,13 @@ public class ValidationItemControllerV2 {
         log.info("target={}", bindingResult.getTarget());
 
         //핃드 관련 검증 로직(FieldError)
-        if (!StringUtils.hasText(item.getItemName())) {
+        /*if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
-        }
+        }*/
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required"); //ValidationUtils 로 처리
+
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.rejectValue("price", "range");
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null); // 해당 값 넣기
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             bindingResult.rejectValue("quantity", "max");
