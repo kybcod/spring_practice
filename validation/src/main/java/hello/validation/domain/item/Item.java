5406437c1e4e3e3b2cb5d6b2d5f34d5dddd9268e
김예빈 <kyb-0215@naver.com>
2025-07-29 14:25:59 +0900

    V4로 초기

--- validation/src/main/java/hello/validation/domain/item/Item.java
+++ validation/src/main/java/hello/validation/domain/item/Item.java
@@ -9,18 +9,9 @@ import org.hibernate.validator.constraints.Range;
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) //수정시에만 적용
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) // 등록, 수정 사용
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class) //등록시에만 적용
     private Integer quantity;
 
     public Item() {
