11ab081f91540ee54e4af4c494e8633b3ec0721f
김예빈 <kyb-0215@naver.com>
2025-07-29 13:38:44 +0900

    @Validated : WebDataBinder에 등록한 검증기를 찾아서 실행

--- validation/src/main/java/hello/validation/ValidationApplication.java
+++ validation/src/main/java/hello/validation/ValidationApplication.java
@@ -1,13 +1,21 @@
 package hello.validation;
 
+import hello.validation.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ValidationApplication {
+public class ValidationApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ValidationApplication.class, args);
 	}
 
+	// 전체 Validate 적용(컨트롤러에서 @InitBinder 부분 제거)
+	@Override
+	public Validator getValidator() {
+		return new ItemValidator();
+	}
 }
