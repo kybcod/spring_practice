ed54f6c01d2d881821e5cadef7447a9f4e99c3ea
김예빈 <kyb-0215@naver.com>
2025-07-31 11:17:28 +0900

    서블릿 예외 처리 - 필터(DispatcherType)

---
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR); //클라이언트 요청, 오류 페이지 요청 필터 호츌
+        return filterRegistrationBean;
+    }
+}
---
+++ exception/src/main/java/hello/exception/filter/LogFilter.java
@@ -0,0 +1,37 @@
+package hello.exception.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        try{
+            log.info("REQUEST [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
+        }catch (Exception e){
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
