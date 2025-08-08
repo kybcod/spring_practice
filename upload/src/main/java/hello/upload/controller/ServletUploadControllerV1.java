8a3b104147ce7ad01e56cadcc41883b396ca874c
김예빈 <kyb-0215@naver.com>
2025-08-08 15:54:39 +0900

    multipart 사용하여 파일 저장

---
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -0,0 +1,37 @@
+package hello.upload.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile1(HttpServletRequest request) throws Exception {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts(); //multipart/form-data 전송 방식에서 각각 나누어진 부분
+        log.info("parts={}", parts);
+
+        return "upload-form";
+    }
+}
